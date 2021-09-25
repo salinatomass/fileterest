7c94cf404e131b47dc1c018439ed5c412272ad9a
Tomás Salina <salinatomass53@gmail.com>
2021-09-25 18:34:16 -0300

    initial structure

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+// Initializations
+const app = express();
+
+// Settings
+
+// Middlewares
+
+// Global Variables
+
+// Routes
+
+// Static files
+
+// Start the server
